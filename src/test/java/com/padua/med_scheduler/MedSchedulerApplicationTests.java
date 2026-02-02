59a4f6033342840556df197aad22ff3b38c257d2
Carlos Padua <carlospadua2311@gmail.com>
2026-02-02 17:34:17 -0300

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/padua/med_scheduler/MedSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padua.med_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
